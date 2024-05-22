d34edeffb9ef9ec56d3242afe337d3fad9bbddec
Haitham Elabd <haithamelabd@outlook.com>
2024-05-22 21:06:29 +0300

    update testing file

--- app/main.py
+++ app/main.py
@@ -3,9 +3,11 @@ import requests
 
 app = FastAPI()
 
+
 @app.get("/version")
 def read_version():
     return {"version": "v0.0.1"}
+
 
 @app.get("/temperature")
 def read_temperature():
@@ -17,25 +19,28 @@ def read_temperature():
     temperatures = []
 
     for box_id in box_ids:
-        response = requests.get(f'https://api.opensensemap.org/boxes/{box_id}?format=json')
+        response = requests.get(
+            f'https://api.opensensemap.org/boxes/{box_id}?format=json'
+        )
         data = response.json()
 
         for sensor in data['sensors']:
             if sensor['title'] == 'Temperatur' and 'lastMeasurement' in sensor:
-                last_measurement = sensor['lastMeasurement']
-                if last_measurement is not None and 'value' in last_measurement:
-                    temperature = float(last_measurement['value'])
+                last_measure = sensor['lastMeasurement']
+                if last_measure is not None and 'value' in last_measure:
+                    temperature = float(last_measure['value'])
                     temperatures.append(temperature)
                     break
         else:
-            # If temperature data is not found for a box, continue to the next box
+            # If temperature data is not found for a box
             continue
-    
+
     if not temperatures:
-        raise HTTPException(status_code=404, detail="Temperature data not found")
+        raise HTTPException(status_code=404, detail="not found")
 
     average_temperature = sum(temperatures) / len(temperatures)
     return {"average_temperature": average_temperature}
+
 
 if __name__ == "__main__":
     import uvicorn
