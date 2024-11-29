9bc2c6c6c1a991c3adb065df904a0a339451381c
haitham <haithamelabd@outlook.com>
2024-11-29 17:06:51 +0200

    add: flake configuration

--- app/main.py
+++ app/main.py
@@ -8,7 +8,10 @@ app = FastAPI()
 # READ SENSEBOX IDS FROM ENVIRONMENT VARIABLES, OR PROVIDE A DEFAULT LIST
 SENSEBOX_IDS = os.getenv(
     "SENSEBOX_IDS",
-    "5eba5fbad46fb8001b799786,5eb99cacd46fb8001b2ce04c,5e60cf5557703e001bdae7f8"
+    (
+        "5eba5fbad46fb8001b799786,5eb99cacd46fb8001b2ce04c,"
+        "5e60cf5557703e001bdae7f8"
+    ),
 ).split(",")
 
 # INITIALIZE THE PROMETHEUS INSTRUMENTATOR
@@ -28,28 +31,29 @@ def read_temperature():
     temperatures = []
     for box_id in SENSEBOX_IDS:
         response = requests.get(
-            f'https://api.opensensemap.org/boxes/{box_id}?format=json'
+            f"https://api.opensensemap.org/boxes/{box_id}?format=json"
         )
         if response.status_code != 200:
             continue
-        
         data = response.json()
-        for sensor in data['sensors']:
-            if sensor['title'] == 'Temperatur' and 'lastMeasurement' in sensor:
-                last_measure = sensor['lastMeasurement']
-                if last_measure is not None and 'value' in last_measure:
-                    temperature = float(last_measure['value'])
+        for sensor in data["sensors"]:
+            if sensor["title"] == "Temperatur" and "lastMeasurement" in sensor:
+                last_measure = sensor["lastMeasurement"]
+                if last_measure is not None and "value" in last_measure:
+                    temperature = float(last_measure["value"])
                     temperatures.append(temperature)
                     break
         else:
             # IF TEMPERATURE DATA IS NOT FOUND FOR A BOX
             continue
-    
+
     if not temperatures:
         raise HTTPException(status_code=404, detail="No temperature data found")
-    
-    average_temperature = sum(temperatures) / len(temperatures)
-    
+
+    average_temperature = (
+        sum(temperatures) / len(temperatures)
+    )
+
     # DETERMINE STATUS BASED ON THE AVERAGE TEMPERATURE
     if average_temperature < 10:
         status = "Too Cold"
@@ -57,13 +61,13 @@ def read_temperature():
         status = "Good"
     else:
         status = "Too Hot"
-    
+
     return {
         "average_temperature": average_temperature,
-        "status": status
+        "status": status,
     }
 
-    
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
